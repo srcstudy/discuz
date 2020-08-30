d485ae0a78abcaf8e07f6eb8789d2afd042094ac
admin@elesos.com <admin@elesos.com>
2020-08-30 20:40:42 +0800

    first blood

---
+++ vendor/franzl/whoops-middleware/src/WhoopsRunner.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Franzl\Middleware\Whoops;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Whoops\Handler\PlainTextHandler;
+use Whoops\Run;
+use Zend\Diactoros\Response\HtmlResponse;
+
+class WhoopsRunner
+{
+    public static function handle($error, ServerRequestInterface $request)
+    {
+        $method = Run::EXCEPTION_HANDLER;
+
+        $format = FormatNegotiator::negotiate($request);
+        $whoops = self::getWhoopsInstance($format);
+
+        // Output is managed by the middleware pipeline
+        $whoops->allowQuit(false);
+        
+        ob_start();
+        $whoops->$method($error);
+        $response = ob_get_clean();
+
+        return new HtmlResponse(
+            $response,
+            500,
+            ['Content-Type' => $format->getPreferredContentType()]
+        );
+    }
+
+    private static function getWhoopsInstance(Formats\Format $format)
+    {
+        $whoops = new Run();
+        if (php_sapi_name() === 'cli') {
+            $whoops->pushHandler(new PlainTextHandler);
+            return $whoops;
+        }
+
+        $whoops->pushHandler($format->getHandler());
+        return $whoops;
+    }
+}
